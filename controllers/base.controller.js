3a710c6a7ebf1f90980b1260eb32a41a3aea53c7
EnriqueBarba <enrique.barbadillo26@gmail.com>
2019-11-07 23:10:57 +0100

    Celebrities list and celebrity details done

--- controllers/base.controller.js
+++ controllers/base.controller.js
@@ -1,18 +1,33 @@
 const createError = require('http-errors');
 const mongoose = require('mongoose');
+const Celebrity = require('../models/celebrity.model')
 
 module.exports.base = (req, res, next) => {
     res.render('index', {
-        title: 'Welcome to your CRUD project'
+        title: 'Welcome to Celebrities and movies CRUD project'
     });
 };
 
 module.exports.listCelebrities = (req, res, next) => {
     console.log(`listCelebrities`)
+    Celebrity.find()
+    .then(celebrities => {
+        res.render('celebrities/celebritiesList', {celebrities})
+    })
+    .catch( err => {
+        next(err)
+    })
 };
 
 module.exports.celebrityDetails = (req, res, next) => {
-    console.log(`celebritieDetails`)
+    console.log(`celebrityDetails`)
+    Celebrity.findById(req.params.id)
+    .then( celebrity => {
+        res.render('celebrities/celebrityDetails', {celebrity})
+    })
+    .catch( err => {
+        next(err)
+    })
 };
 
 module.exports.editCelebrity = (req, res, next) => {
